d602bd10e22bf884a666ed24d2298da915fefe41
mpstaud <98075113+mpstaud@users.noreply.github.com>
2025-01-07 23:36:49 -0600

    add first template

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from models import db, User
 from operations import add_user, get_all_users
@@ -10,9 +10,10 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disable event system for
 
 db.init_app(app)
 
+
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():  # put application's code here
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
@@ -9,6 +10,7 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User {self.name}>'
+
 
 class FoodLog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
