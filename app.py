262a255a27be5b3e56e3b71ed706bd97dc76b3e5
Matt Staudacher <mstaudac@ruan.com>
2025-01-09 12:19:48 -0600

    Add layout to templates folder

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from models import db, User
 from operations import add_user, get_all_users
 
@@ -9,7 +10,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'  # Database fi
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disable event system for performance
 
 db.init_app(app)
-
+migrate = Migrate(app, db)
 
 @app.route('/')
 def home():  # put application's code here
